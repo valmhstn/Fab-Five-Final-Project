70b479672c536a6024a8fde93e782e77cdef8ab2
bethelike98 <87020742+bethelike98@users.noreply.github.com>
2022-05-17 21:09:41 -0700

    Update test.r

--- test.r
+++ test.r
@@ -5,3 +5,10 @@ library(tidyverse)
 wine = read.csv("Data/WineQT.csv")
 
 head(wine)
+
+#Renaming columns by replacing spaces with dots
+names(wine) <- str_replace_all(names(wine), c(" "="."))
+
+#Dropping Id column
+wine = subset(wine, select = -c(Id))
+
